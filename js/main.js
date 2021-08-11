8ac81c03815befad0287f83b07c12eb5c0739c5a
morgunove <morgunove87@gmail.com>
2021-08-11 07:05:01 +0500

    jquery ui

--- js/main.js
+++ js/main.js
@@ -4,37 +4,46 @@ const click = 'Ты кликнул на кнопку'
 const el = '<li>Дополнительный элемент</li>'
 
 $(function() {
-  $('.input-cart-number').numeric()
-  $('#card-ccv').numeric()
-  $('.input-cart-number').on('keyup change', function() {
-    $('.number span:nth-child(' + $(this).index() + ')').text($(this).val())
-    if($(this).val().length > 3) {
-      $(this).next().focus();
+  $('.folder').draggable();
+  $('.basket').droppable({
+    drop: function() {
+      $('.ui-draggable-dragging').remove();
     }
-    if($(this).val().length < 1) {
-      $(this).prev().focus()
-    }
-    if($(this).val().length > 3 && $(this).index() == 4) {
-      $('#card-holder').focus();
-    }
-  })
-  $('#card-holder').on('keyup change', function() {
-    $('.card-holder div').text($(this).val())
-  })
-  $('#card-expiration-month').change(function() {
-    $('.month').text($(this).val() + '/')
-  })
-  $('#card-expiration-year').change(function() {
-    $('.year').text($(this).val())
-  })
-  $('#card-ccv').on('focus', function() {
-    $('.credit-card-box').addClass('hover')
-  }).on('blur', function() {
-    $('.credit-card-box').removeClass('hover')
-  }).on('keyup change', function() {
-    $('.ccv').text($(this).val())
   })
 })
+
+// $(function() {
+//   $('.input-cart-number').numeric()
+//   $('#card-ccv').numeric()
+//   $('.input-cart-number').on('keyup change', function() {
+//     $('.number span:nth-child(' + $(this).index() + ')').text($(this).val())
+//     if($(this).val().length > 3) {
+//       $(this).next().focus();
+//     }
+//     if($(this).val().length < 1) {
+//       $(this).prev().focus()
+//     }
+//     if($(this).val().length > 3 && $(this).index() == 4) {
+//       $('#card-holder').focus();
+//     }
+//   })
+//   $('#card-holder').on('keyup change', function() {
+//     $('.card-holder div').text($(this).val())
+//   })
+//   $('#card-expiration-month').change(function() {
+//     $('.month').text($(this).val() + '/')
+//   })
+//   $('#card-expiration-year').change(function() {
+//     $('.year').text($(this).val())
+//   })
+//   $('#card-ccv').on('focus', function() {
+//     $('.credit-card-box').addClass('hover')
+//   }).on('blur', function() {
+//     $('.credit-card-box').removeClass('hover')
+//   }).on('keyup change', function() {
+//     $('.ccv').text($(this).val())
+//   })
+// })
 
 // $( function() {
 //   let mCount = $( '.m-count' ),
